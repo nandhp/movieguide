5e76ed3db68e75a65e456d48c529b7ec5410427f
nandhp <nandhp@gmail.com>
2013-07-14 10:21:34 -0500

    Implement code to *restore* a database backup. Again.
    Ironically, I lost the original version of this commit due to lack of backups.

--- backup.py
+++ backup.py
@@ -5,7 +5,7 @@
 from subprocess import Popen, PIPE
 from cStringIO import StringIO
 from gzip import GzipFile
-from base64 import urlsafe_b64encode
+from base64 import urlsafe_b64encode, urlsafe_b64decode
 import os.path, shutil, difflib, urllib2, hashlib
 
 def dump(filename):
@@ -24,6 +24,15 @@ def gzip(data):
         gzfh.write(i + '\n')
     gzfh.close()
     buf = compressed_data.getvalue()
+    compressed_data.close()
+    return buf
+
+def gunzip(data):
+    """Uncompress a string with gzip"""
+    compressed_data = StringIO(data)
+    gzfh = GzipFile(mode='r', fileobj=compressed_data)
+    buf = gzfh.read()
+    gzfh.close()
     compressed_data.close()
     return buf
 
@@ -78,7 +87,44 @@ def run_backup(database_file, post_url, post_data,
     shutil.move(temp_file, incr_file)
     return True
 
+def restore_backup(filename, outdir):
+    infh = open(filename, 'r')
+    fnstr = '%05d%s.txt'
+    lastfull = 0
+    lastline = 0
+    # Unpack all rows into data files
+    for lineno, line in enumerate(infh):
+        timestamp, data, datatype, checksum = line.strip().split(',')
+        if datatype not in 'FI':
+            print "Data at timestamp '%s' has invalid datatype '%s'" % \
+                (timestamp, datatype)
+            continue
+        assert(lineno > 0)
+        assert(hashlib.sha1(data).hexdigest() == checksum)
+        outfn = os.path.join(outdir, fnstr % (lineno, datatype))
+        outfh = open(outfn, 'wb')
+        outfh.write(gunzip(urlsafe_b64decode(data)))
+        outfh.close()
+        if datatype == 'F':
+            lastfull = lineno
+        lastline = lineno
+    # Apply patches starting from last full backup
+    outfn = os.path.join(outdir, 'dump.txt')
+    shutil.copy(os.path.join(outdir, fnstr % (lastfull, 'F')), outfn)
+    for lineno in range(lastfull+1, lastline+1):
+        infn = os.path.join(outdir, fnstr % (lineno, 'I'))
+        obj = Popen(['patch', outfn, infn])
+        obj.wait()
+        assert(obj.returncode == 0)
+    print "Restore completed in %s" % outfn
+
 if __name__ == '__main__':
-    POST_URL = 'http://www.myserver.example/backup'
-    POST_DATA = 'd=%(data)s&t=%(type)s&sha1=%(checksum)s'
-    run_backup('movieguide.db', POST_URL, POST_DATA)
+    import sys
+    if sys.argv[1] == 'backup':
+        POST_URL = 'http://www.myserver.example/backup'
+        POST_DATA = 'd=%(data)s&t=%(type)s&sha1=%(checksum)s'
+        run_backup('movieguide.db', POST_URL, POST_DATA)
+    elif sys.argv[1] == 'restore':
+        restore_backup(sys.argv[2], 'restored-backup')
+    else:
+        print "Usage: %s {backup|restore}" % (sys.argv[0],)
